dbecb77d057289bbb126da0b6c0212f8be975417
nareshkrishnaa <nareshkrishnaaa@gmail.com>
2026-04-06 20:13:51 +0530

    added comment line

--- src/main/java/dev/learning/creational/singleton/Driver.java
+++ src/main/java/dev/learning/creational/singleton/Driver.java
@@ -2,10 +2,11 @@ package dev.learning.creational.singleton;
 
 public class Driver {
     static void main() {
-        Singleton singleton = Singleton.getSingleton();
-        System.out.println(singleton.hashCode());;
-
         Singleton singleton1 = Singleton.getSingleton();
+        System.out.println(singleton1.hashCode());;
+
+        Singleton singleton2 = Singleton.getSingleton();
         System.out.println(singleton1.hashCode());
+        // both Singleton variables refer to the same Object
     }
 }
